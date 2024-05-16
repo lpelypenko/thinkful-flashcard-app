458d789dad0314514485eb03884362bcca913c9f
lpelypenko <2556875+lpelypenko@users.noreply.github.com>
2024-05-16 14:46:56 +0900

    test readme

---
+++ src/README.md
@@ -0,0 +1,3 @@
+# Result
+
+![alt text](app-home.png)
--- src/components/common/Icon.js
+++ src/components/common/Icon.js
@@ -10,7 +10,7 @@ function Icon({ type }) {
           width="16"
           height="16"
           fill="currentColor"
-          class="bi bi-plus-lg"
+          className="bi bi-plus-lg"
           viewBox="0 0 16 16"
           style={{
             verticalAlign: "middle",
@@ -33,7 +33,7 @@ function Icon({ type }) {
           width="16"
           height="16"
           fill="currentColor"
-          class="bi bi-trash-fill"
+          className="bi bi-trash-fill"
           viewBox="0 0 16 16"
         >
           <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
@@ -48,7 +48,7 @@ function Icon({ type }) {
           width="16"
           height="16"
           fill="currentColor"
-          class="bi bi-house-door-fill"
+          className="bi bi-house-door-fill"
           viewBox="0 0 16 16"
           style={{
             verticalAlign: "middle",
@@ -68,7 +68,7 @@ function Icon({ type }) {
           width="16"
           height="16"
           fill="currentColor"
-          class="bi bi-pen"
+          className="bi bi-pen"
           viewBox="0 0 16 16"
         >
           <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z" />
@@ -83,7 +83,7 @@ function Icon({ type }) {
           width="16"
           height="16"
           fill="currentColor"
-          class="bi bi-journal-arrow-up"
+          className="bi bi-journal-arrow-up"
           viewBox="0 0 16 16"
         >
           <path
@@ -103,7 +103,7 @@ function Icon({ type }) {
           width="16"
           height="16"
           fill="currentColor"
-          class="bi bi-eye"
+          className="bi bi-eye"
           viewBox="0 0 16 16"
         >
           <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z" />
